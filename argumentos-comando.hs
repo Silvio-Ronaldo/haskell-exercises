c3db12bbd744a9c321ad3a58eacb0cd7f337d4ff
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2020-11-13 21:03:28 -0300

    Recebendo argumentos da linha de comando

---
+++ argumentos-comando.hs
@@ -0,0 +1,10 @@
+import System.Environment 
+import Data.List
+
+main = do
+    args <- getArgs
+    progName <- getProgName
+    putStrLn "Os argumentos são:"
+    mapM putStrLn args
+    putStrLn "O nome do programa é:"
+    putStrLn progName
